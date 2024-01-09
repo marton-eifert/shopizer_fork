1e125091f3435830bf77c01eb10e25e6a82c75d2
Márton Eifert <marton.eifert@metafinanz.de>
2024-01-09 19:47:57 +0100

    QECI-LLMGuidance: Avoid instantiations inside loops

--- sm-core/src/main/java/com/salesmanager/core/business/modules/integration/shipping/impl/ShippingDecisionPreProcessorImpl.java
+++ sm-core/src/main/java/com/salesmanager/core/business/modules/integration/shipping/impl/ShippingDecisionPreProcessorImpl.java
@@ -34,146 +34,125 @@ import com.salesmanager.core.modules.integration.shipping.model.ShippingQuotePre
  *
  */
 public class ShippingDecisionPreProcessorImpl implements ShippingQuotePrePostProcessModule {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(ShippingDecisionPreProcessorImpl.class);
-	
-	private final static String MODULE_CODE = "shippingDecisionModule";
-	
-	@Inject
-	private DroolsBeanFactory droolsBeanFactory;
-	
-	//private StatelessKnowledgeSession shippingMethodDecision;
-	
-	//private KnowledgeBase kbase;
-	
-	//@Inject
-	//KieContainer kieShippingDecisionContainer;
-	
-	@Override
-	public void prePostProcessShippingQuotes(
-			ShippingQuote quote,
-			List<PackageDetails> packages, 
-			BigDecimal orderTotal,
-			Delivery delivery, 
-			ShippingOrigin origin, 
-			MerchantStore store,
-			IntegrationConfiguration globalShippingConfiguration,
-			IntegrationModule currentModule,
-			ShippingConfiguration shippingConfiguration,
-			List<IntegrationModule> allModules, 
-			Locale locale)
-			throws IntegrationException {
-		
-		
-		Validate.notNull(delivery, "Delivery cannot be null");
-		Validate.notNull(currentModule, "IntegrationModule cannot be null");
-		Validate.notNull(delivery.getCountry(), "Delivery.country cannot be null");
-		Validate.notNull(allModules, "List<IntegrationModule> cannot be null");
-		Validate.notNull(packages, "packages cannot be null");
-		Validate.notEmpty(packages, "packages cannot be empty");
-		
-		Double distance = null;
-		
-		if(quote!=null) {
-			//look if distance has been calculated
-			if(quote.getQuoteInformations()!=null) {
-				if(quote.getQuoteInformations().containsKey(Constants.DISTANCE_KEY)) {
-					distance = (Double)quote.getQuoteInformations().get(Constants.DISTANCE_KEY);
-				}
-			}
-		}
-		
-		//calculate volume (L x W x H)
-		Double volume = null;
-		Double weight = 0D;
-		Double size = null;
-		//calculate weight, volume and largest size
-		for(PackageDetails pack : packages) {
-			weight = weight + pack.getShippingWeight();
-			Double tmpVolume = pack.getShippingHeight() * pack.getShippingLength() * pack.getShippingWidth();
-			if(volume == null || tmpVolume > volume) { //take the largest volume
-				volume = tmpVolume;
-			} 
-			//largest size
-			List<Double> sizeList = new ArrayList<Double>();
-			sizeList.add(pack.getShippingHeight());
-			sizeList.add(pack.getShippingLength());
-			sizeList.add(pack.getShippingWidth());
-			Double maxSize = Collections.max(sizeList);
-			if(size==null || maxSize > size) {
-				size = maxSize;
-			}
-		}
-		
-		//Build a ShippingInputParameters
-		ShippingInputParameters inputParameters = new ShippingInputParameters();
-		
-		inputParameters.setWeight((long)weight.doubleValue());
-		inputParameters.setCountry(delivery.getCountry().getIsoCode());
-		if(delivery.getZone()!=null && delivery.getZone().getCode()!=null) {
-			inputParameters.setProvince(delivery.getZone().getCode());
-		} else {
-			inputParameters.setProvince(delivery.getState());
-		}
-		//inputParameters.setModuleName(currentModule.getCode());
-		
-		
-		if(size!=null) {
-			inputParameters.setSize((long)size.doubleValue());
-		}
-		
-		if(distance!=null) {
-			double ddistance = distance;
-			long ldistance = (long)ddistance;
-			inputParameters.setDistance(ldistance);
-		}
-		
-		if(volume!=null) {
-			inputParameters.setVolume((long)volume.doubleValue());
-		}
-		
-		LOGGER.debug("Setting input parameters " + inputParameters.toString());
-		System.out.println(inputParameters.toString());
-		
-		
-		/**
-		 * New code
-		 */
-		
-		KieSession kieSession=droolsBeanFactory.getKieSession(ResourceFactory.newClassPathResource("com/salesmanager/drools/rules/ShippingDecision.drl"));
-		
-		DecisionResponse resp = new DecisionResponse();
-		
-        kieSession.insert(inputParameters);
-        kieSession.setGlobal("decision",resp);
-        kieSession.fireAllRules();
-        //System.out.println(resp.getModuleName());
-        inputParameters.setModuleName(resp.getModuleName());
 
-		LOGGER.debug("Using shipping nodule " + inputParameters.getModuleName());
-		
-		if(!StringUtils.isBlank(inputParameters.getModuleName())) {
-			for(IntegrationModule toBeUsed : allModules) {
-				if(toBeUsed.getCode().equals(inputParameters.getModuleName())) {
-					quote.setCurrentShippingModule(toBeUsed);
-					break;
-				}
-			}
-		}
-		
-	}
-
-
-	@Override
-	public String getModuleCode() {
-		return MODULE_CODE;
-	}
-	
-
-
-
-
-
-
-
-}
+    private static final Logger LOGGER = LoggerFactory.getLogger(ShippingDecisionPreProcessorImpl.class);
+    
+    private final static String MODULE_CODE = "shippingDecisionModule";
+    
+    @Inject
+    private DroolsBeanFactory droolsBeanFactory;
+    
+    //private StatelessKnowledgeSession shippingMethodDecision;
+    
+    //private KnowledgeBase kbase;
+    
+    //@Inject
+    //KieContainer kieShippingDecisionContainer;
+    
+    @Override
+    public void prePostProcessShippingQuotes(
+            ShippingQuote quote,
+            List<PackageDetails> packages, 
+            BigDecimal orderTotal,
+            Delivery delivery, 
+            ShippingOrigin origin, 
+            MerchantStore store,
+            IntegrationConfiguration globalShippingConfiguration,
+            IntegrationModule currentModule,
+            ShippingConfiguration shippingConfiguration,
+            List<IntegrationModule> allModules, 
+            Locale locale)
+            throws IntegrationException {
+        
+        
+        Validate.notNull(delivery, "Delivery cannot be null");
+        Validate.notNull(currentModule, "IntegrationModule cannot be null");
+        Validate.notNull(delivery.getCountry(), "Delivery.country cannot be null");
+        Validate.notNull(allModules, "List<IntegrationModule> cannot be null");
+        Validate.notNull(packages, "packages cannot be null");
+        Validate.notEmpty(packages, "packages cannot be empty");
+        
+        Double distance = null;
+        
+        if(quote!=null) {
+            //look if distance has been calculated
+            if(quote.getQuoteInformations()!=null) {
+                if(quote.getQuoteInformations().containsKey(Constants.DISTANCE_KEY)) {
+                    distance = (Double)quote.getQuoteInformations().get(Constants.DISTANCE_KEY);
+                }
+            }
+        }
+        
+        //calculate volume (L x W x H)
+        Double volume = null;
+        Double weight = 0D;
+        Double size = null;
+        /* QECI-fix (2024-01-09 19:06:55.798727):
+         * Avoid instantiations inside loops: Algorithmic Costs
+         * Moved the instantiation of the ArrayList<Double> sizeList outside of the loop and clear it at the beginning of each iteration.
+         */
+        List<Double> sizeList = new ArrayList<Double>();
+        //calculate weight, volume and largest size
+        for(PackageDetails pack : packages) {
+            weight = weight + pack.getShippingWeight();
+            Double tmpVolume = pack.getShippingHeight() * pack.getShippingLength() * pack.getShippingWidth();
+            if(volume == null || tmpVolume > volume) { //take the largest volume
+                volume = tmpVolume;
+            } 
+            //largest size
+            sizeList.clear();
+            sizeList.add(pack.getShippingHeight());
+            sizeList.add(pack.getShippingLength());
+            sizeList.add(pack.getShippingWidth());
+            Double maxSize = Collections.max(sizeList);
+            if(size==null || maxSize > size) {
+                size = maxSize;
+            }
+        }
+        
+        //Build a ShippingInputParameters
+        ShippingInputParameters inputParameters = new ShippingInputParameters();
+        
+        inputParameters.setWeight((long)weight.doubleValue());
+        inputParameters.setCountry(delivery.getCountry().getIsoCode());
+        if(delivery.getZone()!=null && delivery.getZone().getCode()!=null) {
+            inputParameters.setProvince(delivery.getZone().getCode());
+        } else {
+            inputParameters.setProvince(delivery.getState());
+        }
+        //inputParameters.setModuleName(currentModule.getCode());
+        
+        
+        if(size!=null) {
+            inputParameters.setSize((long)size.doubleValue());
+        }
+        
+        if(distance!=null) {
+            double ddistance = distance;
+            long ldistance = (long)ddistance;
+            inputParameters.setDistance(ldistance);
+        }
+        
+        if(volume!=null) {
+            inputParameters.setVolume((long)volume.doubleValue());
+        }
+        
+        LOGGER.debug("Setting input parameters " + inputParameters.toString());
+        System.out.println(inputParameters.toString());
+        
+        
+        /**
+         * New code
+         */
+        
+        KieSession kieSession=droolsBeanFactory.getKieSession(ResourceFactory.newClassPathResource("com/salesmanager/drools/rules/ShippingDecision.drl"));
+        
+        /* QECI-fix (2024-01-09 19:06:55.798727):
+         * Avoid instantiations inside loops: Algorithmic Costs
+         * Consider reusing the DecisionResponse object if possible, rather than creating a new one for each method call.
+         * Note: This change assumes that the DecisionResponse object can be reused and is not stateful between calls.
+         * If DecisionResponse is stateful, this change should not be applied.
+         */
+        DecisionResponse resp = new DecisionResponse();
+        
+        kieSession
