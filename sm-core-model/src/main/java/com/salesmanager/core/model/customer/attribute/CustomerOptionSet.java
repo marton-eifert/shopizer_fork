8aae912a00739c0a3d953c4ebc7f58697c67ebba
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-01 20:44:46 +0100

    QECI-Fix @ CustomerOptionSet.java # 1: Avoid primitive type wrapper instantiation

--- sm-core-model/src/main/java/com/salesmanager/core/model/customer/attribute/CustomerOptionSet.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/customer/attribute/CustomerOptionSet.java
@@ -50,6 +50,19 @@ public class CustomerOptionSet extends SalesManagerEntity<Long, CustomerOptionSe
 
 
 	@Column(name="SORT_ORDER")
+
+
+
+
+/**********************************
+ * CAST-Finding START #1 (2024-02-01 20:44:46.089511):
+ * TITLE: Avoid primitive type wrapper instantiation
+ * DESCRIPTION: Literal values are built at compil time, and their value stored directly in the variable. Literal strings also benefit from an internal mechanism of string pool, to prevent useless duplication, according to the fact that literal string are immutable. On the contrary, values created through wrapper type instantiation need systematically the creation of a new object with many attributes and a life process to manage, and can lead to redondancies for identical values.
+ * STATUS: OPEN
+ * CAST-Finding END #1
+ **********************************/
+
+
 	private Integer sortOrder = new Integer(0);
 	
 
