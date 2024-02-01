00ac8189b117fa72aa8a6c980bd8415a2e6ba83a
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-01 21:55:07 +0100

    QECI-Fix @ ProductUtils.java # 1: Avoid instantiations inside loops

--- sm-core/src/main/java/com/salesmanager/core/business/utils/ProductUtils.java
+++ sm-core/src/main/java/com/salesmanager/core/business/utils/ProductUtils.java
@@ -14,6 +14,19 @@ public class ProductUtils {
 		StringBuilder attributeName = null;
 		for(OrderProductAttribute oProductAttribute : oAttributes) {
 			if(attributeName == null) {
+
+
+
+
+/**********************************
+ * CAST-Finding START #1 (2024-02-01 21:55:06.937153):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #1
+ **********************************/
+
+
 				attributeName = new StringBuilder();
 				attributeName.append("[");
 			} else {
