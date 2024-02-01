692d49d3e0f48b75d61692a51759398f469edad5
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-01 23:40:12 +0100

    QECI-Fix @ ProductFacadeV2Impl.java # 2: Avoid instantiations inside loops

--- sm-shop/src/main/java/com/salesmanager/shop/store/facade/product/ProductFacadeV2Impl.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/facade/product/ProductFacadeV2Impl.java
@@ -228,7 +228,9 @@ public class ProductFacadeV2Impl implements ProductFacade {
  * CAST-Finding START #1 (2024-02-01 23:39:54.416483):
  * TITLE: Avoid instantiations inside loops
  * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
- * STATUS: OPEN
+ * OUTLINE: The code line `List<ReadableProduct> items = new ArrayList<ReadableProduct>();` is most likely affected. - Reasoning: The instantiation of the `ArrayList` object inside the loop can be moved outside to avoid unnecessary object instantiation at each iteration. - Proposed solution: Move the instantiation of `List<ReadableProduct> items = new ArrayList<ReadableProduct>();` outside the loop.
+ * INSTRUCTION: {instruction}
+ * STATUS: IN_PROGRESS
  * CAST-Finding END #1
  **********************************/
 
