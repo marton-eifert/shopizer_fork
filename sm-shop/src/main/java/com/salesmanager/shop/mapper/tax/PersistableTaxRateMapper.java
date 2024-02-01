8eb1bcd77eba66a5029af060d9e73434e882cfb9
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-01 22:30:24 +0100

    QECI-Fix @ PersistableTaxRateMapper.java # 1: Avoid nested loops

--- sm-shop/src/main/java/com/salesmanager/shop/mapper/tax/PersistableTaxRateMapper.java
+++ sm-shop/src/main/java/com/salesmanager/shop/mapper/tax/PersistableTaxRateMapper.java
@@ -75,6 +75,19 @@ public class PersistableTaxRateMapper implements Mapper<PersistableTaxRate, TaxR
 	          for(TaxRateDescription desc : source.getDescriptions()) {
 	        	com.salesmanager.core.model.tax.taxrate.TaxRateDescription description = null;
 	            if(!CollectionUtils.isEmpty(destination.getDescriptions())) {
+
+
+
+
+/**********************************
+ * CAST-Finding START #1 (2024-02-01 22:30:24.244364):
+ * TITLE: Avoid nested loops
+ * DESCRIPTION: This rule finds all loops containing nested loops.  Nested loops can be replaced by redesigning data with hashmap, or in some contexts, by using specialized high level API...  With hashmap: The literature abounds with documentation to reduce complexity of nested loops by using hashmap.  The principle is the following : having two sets of data, and two nested loops iterating over them. The complexity of a such algorithm is O(n^2). We can replace that by this process : - create an intermediate hashmap summarizing the non-null interaction between elements of both data set. This is a O(n) operation. - execute a loop over one of the data set, inside which the hash indexation to interact with the other data set is used. This is a O(n) operation.  two O(n) algorithms chained are always more efficient than a single O(n^2) algorithm.  Note : if the interaction between the two data sets is a full matrice, the optimization will not work because the O(n^2) complexity will be transferred in the hashmap creation. But it is not the main situation.  Didactic example in Perl technology: both functions do the same job. But the one using hashmap is the most efficient.  my $a = 10000; my $b = 10000;  sub withNestedLoops() {     my $i=0;     my $res;     while ($i < $a) {         print STDERR "$i\n";         my $j=0;         while ($j < $b) {             if ($i==$j) {                 $res = $i*$j;             }             $j++;         }         $i++;     } }  sub withHashmap() {     my %hash = ();          my $j=0;     while ($j < $b) {         $hash{$j} = $i*$i;         $j++;     }          my $i = 0;     while ($i < $a) {         print STDERR "$i\n";         $res = $hash{i};         $i++;     } } # takes ~6 seconds withNestedLoops();  # takes ~1 seconds withHashmap();
+ * STATUS: OPEN
+ * CAST-Finding END #1
+ **********************************/
+
+
 	              for(com.salesmanager.core.model.tax.taxrate.TaxRateDescription d : destination.getDescriptions()) {
 	                if(!StringUtils.isBlank(desc.getLanguage()) && desc.getLanguage().equals(d.getLanguage().getCode())) {
 	              	  d.setDescription(desc.getDescription());
