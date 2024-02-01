9dfc309f71301b3f826c99811857ff6918f94e90
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-01 21:20:42 +0100

    QECI-Fix @ ProductServiceImpl.java # 1: Avoid instantiations inside loops

--- sm-core/src/main/java/com/salesmanager/core/business/services/catalog/product/ProductServiceImpl.java
+++ sm-core/src/main/java/com/salesmanager/core/business/services/catalog/product/ProductServiceImpl.java
@@ -286,6 +286,19 @@ public class ProductServiceImpl extends SalesManagerEntityServiceImpl<Long, Prod
 						image.setProduct(product);
 
 						InputStream inputStream = image.getImage();
+
+
+
+
+/**********************************
+ * CAST-Finding START #1 (2024-02-01 21:20:42.381037):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #1
+ **********************************/
+
+
 						ImageContentFile cmsContentImage = new ImageContentFile();
 						cmsContentImage.setFileName(image.getProductImage());
 						cmsContentImage.setFile(inputStream);
@@ -310,6 +323,19 @@ public class ProductServiceImpl extends SalesManagerEntityServiceImpl<Long, Prod
 						image.setProduct(product);
 
 						InputStream inputStream = image.getImage();
+
+
+
+
+/**********************************
+ * CAST-Finding START #2 (2024-02-01 21:20:42.381037):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #2
+ **********************************/
+
+
 						ImageContentFile cmsContentImage = new ImageContentFile();
 						cmsContentImage.setFileName(image.getProductImage());
 						cmsContentImage.setFile(inputStream);
