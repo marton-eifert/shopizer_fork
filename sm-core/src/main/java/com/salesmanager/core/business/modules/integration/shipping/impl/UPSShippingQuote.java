f81e4bd04270f2f4895272f69c5d9654c6e8a79a
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-01 21:15:39 +0100

    QECI-Fix @ UPSShippingQuote.java # 1: Avoid instantiations inside loops

--- sm-core/src/main/java/com/salesmanager/core/business/modules/integration/shipping/impl/UPSShippingQuote.java
+++ sm-core/src/main/java/com/salesmanager/core/business/modules/integration/shipping/impl/UPSShippingQuote.java
@@ -338,6 +338,19 @@ public class UPSShippingQuote implements ShippingQuoteModule {
 				xmldatabuffer.append("</Code>");
 				xmldatabuffer.append("</UnitOfMeasurement>");
 				xmldatabuffer.append("<Weight>");
+
+
+
+
+/**********************************
+ * CAST-Finding START #1 (2024-02-01 21:15:38.726213):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #1
+ **********************************/
+
+
 				xmldatabuffer.append(new BigDecimal(packageDetail.getShippingWeight())
 						.setScale(1, BigDecimal.ROUND_HALF_UP));
 				xmldatabuffer.append("</Weight>");
@@ -351,14 +364,53 @@ public class UPSShippingQuote implements ShippingQuoteModule {
 				xmldatabuffer.append("</Code>");
 				xmldatabuffer.append("</UnitOfMeasurement>");
 				xmldatabuffer.append("<Length>");
+
+
+
+
+/**********************************
+ * CAST-Finding START #2 (2024-02-01 21:15:38.726213):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #2
+ **********************************/
+
+
 				xmldatabuffer.append(new BigDecimal(packageDetail.getShippingLength())
 						.setScale(2, BigDecimal.ROUND_HALF_UP));
 				xmldatabuffer.append("</Length>");
 				xmldatabuffer.append("<Width>");
+
+
+
+
+/**********************************
+ * CAST-Finding START #3 (2024-02-01 21:15:38.726213):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #3
+ **********************************/
+
+
 				xmldatabuffer.append(new BigDecimal(packageDetail.getShippingWidth())
 						.setScale(2, BigDecimal.ROUND_HALF_UP));
 				xmldatabuffer.append("</Width>");
 				xmldatabuffer.append("<Height>");
+
+
+
+
+/**********************************
+ * CAST-Finding START #4 (2024-02-01 21:15:38.726213):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #4
+ **********************************/
+
+
 				xmldatabuffer.append(new BigDecimal(packageDetail.getShippingHeight())
 						.setScale(2, BigDecimal.ROUND_HALF_UP));
 				xmldatabuffer.append("</Height>");
