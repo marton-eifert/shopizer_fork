1cd765b4bbb1caad3f0591691d7a737d4e4567bf
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-01 22:03:22 +0100

    QECI-Fix @ UserAuthenticationSuccessHandler.java # 3: Use a virtualised environment where possible

--- sm-shop/src/main/java/com/salesmanager/shop/admin/security/UserAuthenticationSuccessHandler.java
+++ sm-shop/src/main/java/com/salesmanager/shop/admin/security/UserAuthenticationSuccessHandler.java
@@ -33,8 +33,8 @@ public class UserAuthenticationSuccessHandler extends AbstractAuthenticatinSucce
  * TITLE: Use a virtualised environment where possible
  * DESCRIPTION: Footprint measurements clearly show that a virtual server is ten times more energy efficient than a physical server. The superfluous capacity of the server can be used by other applications. When creating the architecture of an application, bear in mind that all parts will be virtualized.  Cloud infrastructures comply with the ISO 50001 standard, which respects energy sobriety. Also "Cloudify" resources offers resource pooling.
  * OUTLINE: The code line `protected void redirectAfterSuccess(HttpServletRequest request, HttpServletResponse response) throws Exception {` is most likely affected.  Reasoning: This method is where the redirect is being performed, and the finding suggests that there may be opportunities to optimize the redirect process for energy efficiency.  Proposed solution: Consider optimizing the redirect process to minimize resource waste or increase efficiency. This could involve using a more efficient redirect strategy or finding ways to reduce the energy consumption of the redirect operation.
- * INSTRUCTION: {instruction}
- * STATUS: IN_PROGRESS
+ * INSTRUCTION: Please follow the OUTLINE and conduct the proposed steps with the affected code.
+ * STATUS: REVIEWED
  * CAST-Finding END #1
  **********************************/
 
