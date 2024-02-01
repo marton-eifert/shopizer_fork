b4b0c5f6fb7e37d5eeb6e610ff03f5da2e788d0b
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-01 21:56:32 +0100

    QECI-Fix @ AjaxResponse.java # 1: 1. Avoid nested loops +++ 2. Avoid instantiations inside loops

--- sm-core/src/main/java/com/salesmanager/core/business/utils/ajax/AjaxResponse.java
+++ sm-core/src/main/java/com/salesmanager/core/business/utils/ajax/AjaxResponse.java
@@ -97,10 +97,49 @@ public class AjaxResponse implements JSONAware {
 			int count = 0;
 			for(Map keyValue : this.getData()) {
 				if(dataEntries == null) {
+
+
+
+
+/**********************************
+ * CAST-Finding START #1 (2024-02-01 21:56:31.846016):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #1
+ **********************************/
+
+
 					dataEntries = new StringBuilder();
 				}
+
+
+
+
+/**********************************
+ * CAST-Finding START #2 (2024-02-01 21:56:31.846016):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #2
+ **********************************/
+
+
 				JSONObject data = new JSONObject();
 				Set<String> keys = keyValue.keySet();
+
+
+
+
+/**********************************
+ * CAST-Finding START #3 (2024-02-01 21:56:31.846016):
+ * TITLE: Avoid nested loops
+ * DESCRIPTION: This rule finds all loops containing nested loops.  Nested loops can be replaced by redesigning data with hashmap, or in some contexts, by using specialized high level API...  With hashmap: The literature abounds with documentation to reduce complexity of nested loops by using hashmap.  The principle is the following : having two sets of data, and two nested loops iterating over them. The complexity of a such algorithm is O(n^2). We can replace that by this process : - create an intermediate hashmap summarizing the non-null interaction between elements of both data set. This is a O(n) operation. - execute a loop over one of the data set, inside which the hash indexation to interact with the other data set is used. This is a O(n) operation.  two O(n) algorithms chained are always more efficient than a single O(n^2) algorithm.  Note : if the interaction between the two data sets is a full matrice, the optimization will not work because the O(n^2) complexity will be transferred in the hashmap creation. But it is not the main situation.  Didactic example in Perl technology: both functions do the same job. But the one using hashmap is the most efficient.  my $a = 10000; my $b = 10000;  sub withNestedLoops() {     my $i=0;     my $res;     while ($i < $a) {         print STDERR "$i\n";         my $j=0;         while ($j < $b) {             if ($i==$j) {                 $res = $i*$j;             }             $j++;         }         $i++;     } }  sub withHashmap() {     my %hash = ();          my $j=0;     while ($j < $b) {         $hash{$j} = $i*$i;         $j++;     }          my $i = 0;     while ($i < $a) {         print STDERR "$i\n";         $res = $hash{i};         $i++;     } } # takes ~6 seconds withNestedLoops();  # takes ~1 seconds withHashmap();
+ * STATUS: OPEN
+ * CAST-Finding END #3
+ **********************************/
+
+
 				for(String key : keys) {
 					data.put(key, keyValue.get(key));
 				}
@@ -124,6 +163,19 @@ public class AjaxResponse implements JSONAware {
 			int count = 0;
 			for(String key : this.getDataMap().keySet()) {
 				if(dataEntries == null) {
+
+
+
+
+/**********************************
+ * CAST-Finding START #4 (2024-02-01 21:56:31.846016):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #4
+ **********************************/
+
+
 					dataEntries = new StringBuilder();
 				}
 				
@@ -147,6 +199,19 @@ public class AjaxResponse implements JSONAware {
 			int count = 0;
 			for(String key : this.getValidationMessages().keySet()) {
 				if(dataEntries == null) {
+
+
+
+
+/**********************************
+ * CAST-Finding START #5 (2024-02-01 21:56:31.846016):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #5
+ **********************************/
+
+
 					dataEntries = new StringBuilder();
 				}
 				dataEntries.append("{");
