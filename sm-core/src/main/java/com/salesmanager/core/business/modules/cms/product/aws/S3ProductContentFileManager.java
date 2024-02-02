d550330d81145dbded6bda84aaf13bcffcf6ed77
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-02 12:30:42 +0100

    QECI-Fix @ S3ProductContentFileManager.java # 1: Avoid instantiations inside loops

--- sm-core/src/main/java/com/salesmanager/core/business/modules/cms/product/aws/S3ProductContentFileManager.java
+++ sm-core/src/main/java/com/salesmanager/core/business/modules/cms/product/aws/S3ProductContentFileManager.java
@@ -93,14 +93,53 @@ public class S3ProductContentFileManager
       List<S3ObjectSummary> objects = results.getObjectSummaries();
       for (S3ObjectSummary os : objects) {
         if (files == null) {
+
+
+
+
+/**********************************
+ * CAST-Finding START #1 (2024-02-02 12:30:41.601250):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #1
+ **********************************/
+
+
           files = new ArrayList<OutputContentFile>();
         }
         String mimetype = URLConnection.guessContentTypeFromName(os.getKey());
         if (!StringUtils.isBlank(mimetype)) {
           S3Object o = s3.getObject(bucketName, os.getKey());
           byte[] byteArray = IOUtils.toByteArray(o.getObjectContent());
+
+
+
+
+/**********************************
+ * CAST-Finding START #2 (2024-02-02 12:30:41.601250):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #2
+ **********************************/
+
+
           ByteArrayOutputStream baos = new ByteArrayOutputStream(byteArray.length);
           baos.write(byteArray, 0, byteArray.length);
+
+
+
+
+/**********************************
+ * CAST-Finding START #3 (2024-02-02 12:30:41.601250):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #3
+ **********************************/
+
+
           OutputContentFile ct = new OutputContentFile();
           ct.setFile(baos);
           files.add(ct);
