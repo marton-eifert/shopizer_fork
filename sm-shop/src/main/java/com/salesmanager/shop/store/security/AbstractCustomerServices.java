d9100902ce5f019eca59cc6b95d53d2c5e2e3f78
Márton Eifert <99172232+marton-eifert@users.noreply.github.com>
2024-02-02 17:35:03 +0100

    Update AbstractCustomerServices.java
    WITHDRAWN

--- sm-shop/src/main/java/com/salesmanager/shop/store/security/AbstractCustomerServices.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/security/AbstractCustomerServices.java
@@ -79,18 +79,15 @@ public abstract class AbstractCustomerServices implements UserDetailsService{
 		    	List<Permission> permissions = permissionService.getPermissions(groupsId);
 		    	for(Permission permission : permissions) {
 
+				/**********************************
+				 * CAST-Finding START #1 (2024-02-02 12:31:10.437681):
+				 * TITLE: Avoid instantiations inside loops
+				 * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+				 * STATUS: WITHDRAWN
+				 * CAST-Finding END #1
+				 **********************************/
 
-
-
-/**********************************
- * CAST-Finding START #1 (2024-02-02 12:31:10.437681):
- * TITLE: Avoid instantiations inside loops
- * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
- * STATUS: OPEN
- * CAST-Finding END #1
- **********************************/
-
-
+				// Instantiation inside loop is valid here
 		    		GrantedAuthority auth = new SimpleGrantedAuthority(permission.getPermissionName());
 		    		authorities.add(auth);
 		    	}
