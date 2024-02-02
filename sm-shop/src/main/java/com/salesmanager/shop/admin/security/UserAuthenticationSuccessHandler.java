a7497def9e8499be8c3ca4db5975b190ae581ab9
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-02 12:30:53 +0100

    QECI-Fix @ UserAuthenticationSuccessHandler.java # 1: Use a virtualised environment where possible

--- sm-shop/src/main/java/com/salesmanager/shop/admin/security/UserAuthenticationSuccessHandler.java
+++ sm-shop/src/main/java/com/salesmanager/shop/admin/security/UserAuthenticationSuccessHandler.java
@@ -24,6 +24,19 @@ public class UserAuthenticationSuccessHandler extends AbstractAuthenticatinSucce
 
 		@Override
 		protected void redirectAfterSuccess(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+
+
+
+/**********************************
+ * CAST-Finding START #1 (2024-02-02 12:30:53.437779):
+ * TITLE: Use a virtualised environment where possible
+ * DESCRIPTION: Footprint measurements clearly show that a virtual server is ten times more energy efficient than a physical server. The superfluous capacity of the server can be used by other applications. When creating the architecture of an application, bear in mind that all parts will be virtualized.  Cloud infrastructures comply with the ISO 50001 standard, which respects energy sobriety. Also "Cloudify" resources offers resource pooling.
+ * STATUS: OPEN
+ * CAST-Finding END #1
+ **********************************/
+
+
 			redirectStrategy.sendRedirect(request, response, "/admin/home.html");
 			
 		}
