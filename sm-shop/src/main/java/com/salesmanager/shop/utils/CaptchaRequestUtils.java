368f69f3678ff3e2f729ecec328160cff9f3db88
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-02 12:31:12 +0100

    QECI-Fix @ CaptchaRequestUtils.java # 1: Avoid primitive type wrapper instantiation

--- sm-shop/src/main/java/com/salesmanager/shop/utils/CaptchaRequestUtils.java
+++ sm-shop/src/main/java/com/salesmanager/shop/utils/CaptchaRequestUtils.java
@@ -80,6 +80,19 @@ public class CaptchaRequestUtils {
 	      // Use caution: ensure correct character encoding and is not binary data
 	      //System.out.println(new String(responseBody));
 	      
+
+
+
+
+/**********************************
+ * CAST-Finding START #1 (2024-02-02 12:31:11.970254):
+ * TITLE: Avoid primitive type wrapper instantiation
+ * DESCRIPTION: Literal values are built at compil time, and their value stored directly in the variable. Literal strings also benefit from an internal mechanism of string pool, to prevent useless duplication, according to the fact that literal string are immutable. On the contrary, values created through wrapper type instantiation need systematically the creation of a new object with many attributes and a life process to manage, and can lead to redondancies for identical values.
+ * STATUS: OPEN
+ * CAST-Finding END #1
+ **********************************/
+
+
 	      String json = new String(responseBody);
 	      
 	      Map<String,String> map = new HashMap<String,String>();
