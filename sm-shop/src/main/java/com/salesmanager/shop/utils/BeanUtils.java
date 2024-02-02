5734458c2088dc85001171bacc6b7f9c4f567e7a
Márton Eifert <99172232+marton-eifert@users.noreply.github.com>
2024-02-02 17:38:12 +0100

    Update BeanUtils.java
    RESOLVED

--- sm-shop/src/main/java/com/salesmanager/shop/utils/BeanUtils.java
+++ sm-shop/src/main/java/com/salesmanager/shop/utils/BeanUtils.java
@@ -51,18 +51,23 @@ public class BeanUtils
         PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
         PropertyDescriptor propertyDescriptor = null;
 
+        /**********************************
+         * CAST-Finding START #1 (2024-02-02 12:31:11.447355):
+         * TITLE: Prefer comparison-to-0 in loop conditions
+         * DESCRIPTION: The loop condition is evaluated at each iteration. The most efficient the test is, the more CPU will be saved.  Comparing against zero is often faster than comparing against other numbers. This isn't because comparison to zero is hardwire in the microprocessor. Zero is the only number where all the bits are off, and the micros are optimized to check this value.  A decreasing loop of integers in which the condition statement is a comparison to zero, will then be faster than the same increasing loop whose condition is a comparison to a non null value.  This rule searches simple conditions (without logical operators for compound conditions ) using comparison operator with two non-zero operands.
+         * STATUS: RESOLVED
+         * CAST-Finding END #1
+         **********************************/
 
-
-
-/**********************************
- * CAST-Finding START #1 (2024-02-02 12:31:11.447355):
- * TITLE: Prefer comparison-to-0 in loop conditions
- * DESCRIPTION: The loop condition is evaluated at each iteration. The most efficient the test is, the more CPU will be saved.  Comparing against zero is often faster than comparing against other numbers. This isn't because comparison to zero is hardwire in the microprocessor. Zero is the only number where all the bits are off, and the micros are optimized to check this value.  A decreasing loop of integers in which the condition statement is a comparison to zero, will then be faster than the same increasing loop whose condition is a comparison to a non null value.  This rule searches simple conditions (without logical operators for compound conditions ) using comparison operator with two non-zero operands.
- * STATUS: OPEN
- * CAST-Finding END #1
- **********************************/
-
-
+        // QECI Fix: Reverse loop with comparison-to-0 (and break after if condition met)
+        for (int i = propertyDescriptors.length - 1; i >= 0; i--) {
+            PropertyDescriptor currentPropertyDescriptor = propertyDescriptors[i];
+            if (currentPropertyDescriptor.getName().equals(propertyname)) {
+                propertyDescriptor = currentPropertyDescriptor;
+                break; // Once the property is found, exit the loop
+            }
+        }
+/*
         for ( int i = 0; i < propertyDescriptors.length; i++ )
         {
             PropertyDescriptor currentPropertyDescriptor = propertyDescriptors[i];
@@ -72,6 +77,7 @@ public class BeanUtils
             }
 
         }
+*/
         return propertyDescriptor;
     }
     
