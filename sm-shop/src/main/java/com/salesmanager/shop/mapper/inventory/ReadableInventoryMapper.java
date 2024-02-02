c5f96128cf0ee4ebe16de2948a9fdfb3473e37d1
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-02 12:30:58 +0100

    QECI-Fix @ ReadableInventoryMapper.java # 1: Avoid instantiations inside loops

--- sm-shop/src/main/java/com/salesmanager/shop/mapper/inventory/ReadableInventoryMapper.java
+++ sm-shop/src/main/java/com/salesmanager/shop/mapper/inventory/ReadableInventoryMapper.java
@@ -122,8 +122,34 @@ public class ReadableInventoryMapper implements Mapper<ProductAvailability, Read
 
 		for (ProductPrice price : source.getPrices()) {
 
+
+
+
+
+/**********************************
+ * CAST-Finding START #1 (2024-02-02 12:30:57.975217):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #1
+ **********************************/
+
+
 			populator = new ReadableProductPricePopulator();
 			populator.setPricingService(pricingService);
+
+
+
+
+/**********************************
+ * CAST-Finding START #2 (2024-02-02 12:30:57.975217):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #2
+ **********************************/
+
+
 			ReadableProductPrice p = populator.populate(price, new ReadableProductPrice(), store, language);
 			prices.add(p);
 
