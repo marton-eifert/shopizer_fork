b381a79c869db631fa1137094938fa6d35068c91
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-02 12:30:47 +0100

    QECI-Fix @ ProductImageServiceImpl.java # 1: Avoid instantiations inside loops

--- sm-core/src/main/java/com/salesmanager/core/business/services/catalog/product/image/ProductImageServiceImpl.java
+++ sm-core/src/main/java/com/salesmanager/core/business/services/catalog/product/image/ProductImageServiceImpl.java
@@ -61,6 +61,19 @@ public class ProductImageServiceImpl extends SalesManagerEntityServiceImpl<Long,
 				Assert.notNull(productImage.getImage());
 
 				InputStream inputStream = productImage.getImage();
+
+
+
+
+/**********************************
+ * CAST-Finding START #1 (2024-02-02 12:30:47.490895):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #1
+ **********************************/
+
+
 				ImageContentFile cmsContentImage = new ImageContentFile();
 				cmsContentImage.setFileName(productImage.getProductImage());
 				cmsContentImage.setFile(inputStream);
