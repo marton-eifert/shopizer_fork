b9101f20088d92c94c1b2e6eecf7e8fb74ca8df2
Márton Eifert <99172232+marton-eifert@users.noreply.github.com>
2024-02-02 12:35:41 +0100

    Update Product.java
    RESOLVED

--- sm-core-model/src/main/java/com/salesmanager/core/model/catalog/product/Product.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/catalog/product/Product.java
@@ -190,13 +190,15 @@ public class Product extends SalesManagerEntity<Long, Product> implements Audita
  * CAST-Finding START #1 (2024-02-02 12:30:38.344287):
  * TITLE: Avoid primitive type wrapper instantiation
  * DESCRIPTION: Literal values are built at compil time, and their value stored directly in the variable. Literal strings also benefit from an internal mechanism of string pool, to prevent useless duplication, according to the fact that literal string are immutable. On the contrary, values created through wrapper type instantiation need systematically the creation of a new object with many attributes and a life process to manage, and can lead to redondancies for identical values.
- * STATUS: OPEN
+ * STATUS: RESOLVED
  * CAST-Finding END #1
  **********************************/
 
 
 	@Column(name = "SORT_ORDER")
-	private Integer sortOrder = new Integer(0);
+	// QECI Fix
+	private Integer sortOrder = 0;
+	// private Integer sortOrder = new Integer(0);
 
 	@NotEmpty
 	@Pattern(regexp="^[a-zA-Z0-9_]*$")
