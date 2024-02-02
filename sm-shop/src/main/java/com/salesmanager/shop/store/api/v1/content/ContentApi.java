62aee75cdc3a4095aa44dd5cbe89f14f91f6a7c4
Márton Eifert <99172232+marton-eifert@users.noreply.github.com>
2024-02-02 17:12:19 +0100

    Update ContentApi.java
    WITHDRAWN

--- sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/content/ContentApi.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/content/ContentApi.java
@@ -441,18 +441,15 @@ public class ContentApi {
 
 		for (MultipartFile f : files) {
 
-
-
-
-/**********************************
- * CAST-Finding START #1 (2024-02-02 12:31:05.250529):
- * TITLE: Avoid instantiations inside loops
- * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
- * STATUS: OPEN
- * CAST-Finding END #1
- **********************************/
-
-
+			/**********************************
+			 * CAST-Finding START #1 (2024-02-02 12:31:05.250529):
+			 * TITLE: Avoid instantiations inside loops
+			 * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+			 * STATUS: WITHDRAWN
+			 * CAST-Finding END #1
+			 **********************************/
+			
+			// Instantiation inside loop is valid here
 			ContentFile cf = new ContentFile();
 			cf.setContentType(f.getContentType());
 			cf.setName(f.getName());
@@ -461,18 +458,15 @@ public class ContentApi {
 				contentFacade.addContentFile(cf, merchantStore.getCode());
 			} catch (IOException e) {
 
-
-
-
-/**********************************
- * CAST-Finding START #2 (2024-02-02 12:31:05.251529):
- * TITLE: Avoid instantiations inside loops
- * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
- * STATUS: OPEN
- * CAST-Finding END #2
- **********************************/
-
-
+				/**********************************
+				 * CAST-Finding START #2 (2024-02-02 12:31:05.251529):
+				 * TITLE: Avoid instantiations inside loops
+				 * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+				 * STATUS: WITHDRAWN
+				 * CAST-Finding END #2
+				 **********************************/
+				
+				// Instantiation inside loop is valid here
 				throw new ServiceRuntimeException("Error while getting file bytes");
 			}
 		}
