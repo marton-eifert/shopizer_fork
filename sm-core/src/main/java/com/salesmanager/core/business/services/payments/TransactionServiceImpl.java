f89639c3dc913f23d0bd7cd0217802070fe29e96
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-02 12:30:49 +0100

    QECI-Fix @ TransactionServiceImpl.java # 1: Avoid instantiations inside loops

--- sm-core/src/main/java/com/salesmanager/core/business/services/payments/TransactionServiceImpl.java
+++ sm-core/src/main/java/com/salesmanager/core/business/services/payments/TransactionServiceImpl.java
@@ -60,6 +60,19 @@ public class TransactionServiceImpl  extends SalesManagerEntityServiceImpl<Long,
 						Map<String,String> objects = mapper.readValue(transaction.getDetails(), Map.class);
 						transaction.setTransactionDetails(objects);
 					} catch (Exception e) {
+
+
+
+
+/**********************************
+ * CAST-Finding START #1 (2024-02-02 12:30:48.752645):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #1
+ **********************************/
+
+
 						throw new ServiceException(e);
 					}
 				}
@@ -130,6 +143,19 @@ public class TransactionServiceImpl  extends SalesManagerEntityServiceImpl<Long,
 						transaction.setTransactionDetails(objects);
 						capturable = transaction;
 					} catch (Exception e) {
+
+
+
+
+/**********************************
+ * CAST-Finding START #2 (2024-02-02 12:30:48.752645):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #2
+ **********************************/
+
+
 						throw new ServiceException(e);
 					}
 				}
