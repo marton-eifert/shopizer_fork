4bbe43ab917318b7bf2ef90bfb1b440bf7d7873f
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-02 12:30:50 +0100

    QECI-Fix @ IntegrationModulesLoader.java # 1: Avoid instantiations inside loops

--- sm-core/src/main/java/com/salesmanager/core/business/services/reference/loader/IntegrationModulesLoader.java
+++ sm-core/src/main/java/com/salesmanager/core/business/services/reference/loader/IntegrationModulesLoader.java
@@ -128,6 +128,19 @@ public class IntegrationModulesLoader {
 	        		
 	        		String env = (String)values.get("env");
 	        		
+
+
+
+
+/**********************************
+ * CAST-Finding START #1 (2024-02-02 12:30:49.896425):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #1
+ **********************************/
+
+
 	        		ModuleConfig config = new ModuleConfig();
 	        		config.setScheme((String)values.get("scheme"));
 	        		config.setHost((String)values.get("host"));
