c46ec2c42e5575639cedc8f55717c7ef636ce359
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-02 12:31:03 +0100

    QECI-Fix @ ReadableOrderPopulator.java # 1: Avoid instantiations inside loops

--- sm-shop/src/main/java/com/salesmanager/shop/populator/order/ReadableOrderPopulator.java
+++ sm-shop/src/main/java/com/salesmanager/shop/populator/order/ReadableOrderPopulator.java
@@ -106,6 +106,19 @@ public class ReadableOrderPopulator extends
 		
 		if(source.getOrderAttributes()!=null && source.getOrderAttributes().size()>0) {
 			for(OrderAttribute attr : source.getOrderAttributes()) {
+
+
+
+
+/**********************************
+ * CAST-Finding START #1 (2024-02-02 12:31:02.580254):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #1
+ **********************************/
+
+
 				com.salesmanager.shop.model.order.OrderAttribute a = new com.salesmanager.shop.model.order.OrderAttribute();
 				a.setKey(attr.getKey());
 				a.setValue(attr.getValue());
