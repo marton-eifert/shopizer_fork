590955a266977b99eb510d60d0e0b067cccf3243
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-05 13:39:34 +0100

    QECI-Fix @ PersistableProductDefinitionMapper.java # 1: 1. Avoid nested loops +++ 2. Avoid string concatenation in loops

--- sm-shop/src/main/java/com/salesmanager/shop/mapper/catalog/product/PersistableProductDefinitionMapper.java
+++ sm-shop/src/main/java/com/salesmanager/shop/mapper/catalog/product/PersistableProductDefinitionMapper.java
@@ -125,9 +125,35 @@ public class PersistableProductDefinitionMapper implements Mapper<PersistablePro
 				  ProductDescription productDescription = new ProductDescription();
 				  Language lang = languageService.getByCode(description.getLanguage());
 	              if(lang==null) {
+
+
+
+
+/**********************************
+ * CAST-Finding START #1 (2024-02-05 13:39:34.302667):
+ * TITLE: Avoid string concatenation in loops
+ * DESCRIPTION: Avoid string concatenation inside loops.  Since strings are immutable, concatenation is a greedy operation. This creates unnecessary temporary objects and results in quadratic rather than linear running time. In a loop, instead using concatenation, add each substring to a list and join the list after the loop terminates (or, write each substring to a byte buffer).
+ * STATUS: OPEN
+ * CAST-Finding END #1
+ **********************************/
+
+
 	                    throw new ConversionException("Language code " + description.getLanguage() + " is invalid, use ISO code (en, fr ...)");
 	               }
 				   if(!CollectionUtils.isEmpty(destination.getDescriptions())) {
+
+
+
+
+/**********************************
+ * CAST-Finding START #2 (2024-02-05 13:39:34.302667):
+ * TITLE: Avoid nested loops
+ * DESCRIPTION: This rule finds all loops containing nested loops.  Nested loops can be replaced by redesigning data with hashmap, or in some contexts, by using specialized high level API...  With hashmap: The literature abounds with documentation to reduce complexity of nested loops by using hashmap.  The principle is the following : having two sets of data, and two nested loops iterating over them. The complexity of a such algorithm is O(n^2). We can replace that by this process : - create an intermediate hashmap summarizing the non-null interaction between elements of both data set. This is a O(n) operation. - execute a loop over one of the data set, inside which the hash indexation to interact with the other data set is used. This is a O(n) operation.  two O(n) algorithms chained are always more efficient than a single O(n^2) algorithm.  Note : if the interaction between the two data sets is a full matrice, the optimization will not work because the O(n^2) complexity will be transferred in the hashmap creation. But it is not the main situation.  Didactic example in Perl technology: both functions do the same job. But the one using hashmap is the most efficient.  my $a = 10000; my $b = 10000;  sub withNestedLoops() {     my $i=0;     my $res;     while ($i < $a) {         print STDERR "$i\n";         my $j=0;         while ($j < $b) {             if ($i==$j) {                 $res = $i*$j;             }             $j++;         }         $i++;     } }  sub withHashmap() {     my %hash = ();          my $j=0;     while ($j < $b) {         $hash{$j} = $i*$i;         $j++;     }          my $i = 0;     while ($i < $a) {         print STDERR "$i\n";         $res = $hash{i};         $i++;     } } # takes ~6 seconds withNestedLoops();  # takes ~1 seconds withHashmap();
+ * STATUS: OPEN
+ * CAST-Finding END #2
+ **********************************/
+
+
 				      for(ProductDescription desc : destination.getDescriptions()) {
 				        if(desc.getLanguage().getCode().equals(description.getLanguage())) {
 				          productDescription = desc;
@@ -165,6 +191,19 @@ public class PersistableProductDefinitionMapper implements Mapper<PersistablePro
 		    if(!CollectionUtils.isEmpty(destination.getAvailabilities())) {
 		      for(ProductAvailability avail : destination.getAvailabilities()) {
 			        Set<ProductPrice> prices = avail.getPrices();
+
+
+
+
+/**********************************
+ * CAST-Finding START #3 (2024-02-05 13:39:34.302667):
+ * TITLE: Avoid nested loops
+ * DESCRIPTION: This rule finds all loops containing nested loops.  Nested loops can be replaced by redesigning data with hashmap, or in some contexts, by using specialized high level API...  With hashmap: The literature abounds with documentation to reduce complexity of nested loops by using hashmap.  The principle is the following : having two sets of data, and two nested loops iterating over them. The complexity of a such algorithm is O(n^2). We can replace that by this process : - create an intermediate hashmap summarizing the non-null interaction between elements of both data set. This is a O(n) operation. - execute a loop over one of the data set, inside which the hash indexation to interact with the other data set is used. This is a O(n) operation.  two O(n) algorithms chained are always more efficient than a single O(n^2) algorithm.  Note : if the interaction between the two data sets is a full matrice, the optimization will not work because the O(n^2) complexity will be transferred in the hashmap creation. But it is not the main situation.  Didactic example in Perl technology: both functions do the same job. But the one using hashmap is the most efficient.  my $a = 10000; my $b = 10000;  sub withNestedLoops() {     my $i=0;     my $res;     while ($i < $a) {         print STDERR "$i\n";         my $j=0;         while ($j < $b) {             if ($i==$j) {                 $res = $i*$j;             }             $j++;         }         $i++;     } }  sub withHashmap() {     my %hash = ();          my $j=0;     while ($j < $b) {         $hash{$j} = $i*$i;         $j++;     }          my $i = 0;     while ($i < $a) {         print STDERR "$i\n";         $res = $hash{i};         $i++;     } } # takes ~6 seconds withNestedLoops();  # takes ~1 seconds withHashmap();
+ * STATUS: OPEN
+ * CAST-Finding END #3
+ **********************************/
+
+
 			        for(ProductPrice p : prices) {
 			          if(p.isDefaultPrice()) {
 			            if(productAvailability == null) {
@@ -275,8 +314,34 @@ public class PersistableProductDefinitionMapper implements Mapper<PersistablePro
 					
 					if(c==null) {
 						if(!StringUtils.isBlank(categ.getCode())) {
+
+
+
+
+/**********************************
+ * CAST-Finding START #4 (2024-02-05 13:39:34.302667):
+ * TITLE: Avoid string concatenation in loops
+ * DESCRIPTION: Avoid string concatenation inside loops.  Since strings are immutable, concatenation is a greedy operation. This creates unnecessary temporary objects and results in quadratic rather than linear running time. In a loop, instead using concatenation, add each substring to a list and join the list after the loop terminates (or, write each substring to a byte buffer).
+ * STATUS: OPEN
+ * CAST-Finding END #4
+ **********************************/
+
+
 							throw new ConversionException("Category code " + categ.getCode() + " does not exist");
 						} else {
+
+
+
+
+/**********************************
+ * CAST-Finding START #5 (2024-02-05 13:39:34.302667):
+ * TITLE: Avoid string concatenation in loops
+ * DESCRIPTION: Avoid string concatenation inside loops.  Since strings are immutable, concatenation is a greedy operation. This creates unnecessary temporary objects and results in quadratic rather than linear running time. In a loop, instead using concatenation, add each substring to a list and join the list after the loop terminates (or, write each substring to a byte buffer).
+ * STATUS: OPEN
+ * CAST-Finding END #5
+ **********************************/
+
+
 							throw new ConversionException("Category id " + categ.getId() + " does not exist");
 
 						}
