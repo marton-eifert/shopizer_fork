139053c19cdf3d2b748da93e6b95054d96009535
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-06 09:25:33 +0100

    QECI-Fix @ PersistableCategoryPopulator.java # 1: 1. Avoid nested loops +++ 2. Avoid instantiations inside loops

--- sm-shop/src/main/java/com/salesmanager/shop/populator/catalog/PersistableCategoryPopulator.java
+++ sm-shop/src/main/java/com/salesmanager/shop/populator/catalog/PersistableCategoryPopulator.java
@@ -106,6 +106,19 @@ public class PersistableCategoryPopulator extends
 
 			for(PersistableCategory cat : source.getChildren()) {
 
+
+
+
+
+/**********************************
+ * CAST-Finding START #1 (2024-02-06 09:25:32.981076):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #1
+ **********************************/
+
+
 				Category persistCategory = this.populate(cat, new Category(), store, language);
 				target.getCategories().add(persistCategory);
 
@@ -118,8 +131,34 @@ public class PersistableCategoryPopulator extends
 			Set<com.salesmanager.core.model.catalog.category.CategoryDescription> descriptions = new HashSet<com.salesmanager.core.model.catalog.category.CategoryDescription>();
 			if(CollectionUtils.isNotEmpty(target.getDescriptions())) {
     			for(com.salesmanager.core.model.catalog.category.CategoryDescription description : target.getDescriptions()) {
+
+
+
+
+/**********************************
+ * CAST-Finding START #2 (2024-02-06 09:25:32.981076):
+ * TITLE: Avoid nested loops
+ * DESCRIPTION: This rule finds all loops containing nested loops.  Nested loops can be replaced by redesigning data with hashmap, or in some contexts, by using specialized high level API...  With hashmap: The literature abounds with documentation to reduce complexity of nested loops by using hashmap.  The principle is the following : having two sets of data, and two nested loops iterating over them. The complexity of a such algorithm is O(n^2). We can replace that by this process : - create an intermediate hashmap summarizing the non-null interaction between elements of both data set. This is a O(n) operation. - execute a loop over one of the data set, inside which the hash indexation to interact with the other data set is used. This is a O(n) operation.  two O(n) algorithms chained are always more efficient than a single O(n^2) algorithm.  Note : if the interaction between the two data sets is a full matrice, the optimization will not work because the O(n^2) complexity will be transferred in the hashmap creation. But it is not the main situation.  Didactic example in Perl technology: both functions do the same job. But the one using hashmap is the most efficient.  my $a = 10000; my $b = 10000;  sub withNestedLoops() {     my $i=0;     my $res;     while ($i < $a) {         print STDERR "$i\n";         my $j=0;         while ($j < $b) {             if ($i==$j) {                 $res = $i*$j;             }             $j++;         }         $i++;     } }  sub withHashmap() {     my %hash = ();          my $j=0;     while ($j < $b) {         $hash{$j} = $i*$i;         $j++;     }          my $i = 0;     while ($i < $a) {         print STDERR "$i\n";         $res = $hash{i};         $i++;     } } # takes ~6 seconds withNestedLoops();  # takes ~1 seconds withHashmap();
+ * STATUS: OPEN
+ * CAST-Finding END #2
+ **********************************/
+
+
     			    for(CategoryDescription d : source.getDescriptions()) {
     			        if(StringUtils.isBlank(d.getLanguage())) {
+
+
+
+
+/**********************************
+ * CAST-Finding START #3 (2024-02-06 09:25:32.981076):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #3
+ **********************************/
+
+
     			          throw new ConversionException("Source category description has no language");
     			        }
     			        if(d.getLanguage().equals(description.getLanguage().getCode())) {
@@ -132,6 +171,19 @@ public class PersistableCategoryPopulator extends
 
 			} else {
 			  for(CategoryDescription d : source.getDescriptions()) {
+
+
+
+
+/**********************************
+ * CAST-Finding START #4 (2024-02-06 09:25:32.981076):
+ * TITLE: Avoid instantiations inside loops
+ * DESCRIPTION: Object instantiation uses memory allocation, that is a greedy operation. Doing an instantiation at each iteration could really hamper the performances and increase resource usage.  If the instantiated object is local to the loop, there is absolutely no need to instantiate it at each iteration : create it once outside the loop, and just change its value at each iteration. If the object is immutable, create if possible a mutable class. If the aim is to create a consolidated data structure, then, unless the need is to release the data case by case, it could be better to make a single global allocation outside the loop, and fill it with data inside the loop.
+ * STATUS: OPEN
+ * CAST-Finding END #4
+ **********************************/
+
+
                 com.salesmanager.core.model.catalog.category.CategoryDescription t = new com.salesmanager.core.model.catalog.category.CategoryDescription();
 
 			    this.buildDescription(d, t);
