6f8ffd106aaa151094a63af606042e2e8c8703a2
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-06 14:04:55 +0100

    QECI-Fix @ Product.java # 1: Avoid primitive type wrapper instantiation

--- sm-core-model/src/main/java/com/salesmanager/core/model/catalog/product/Product.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/catalog/product/Product.java
@@ -180,6 +180,19 @@ public class Product extends SalesManagerEntity<Long, Product> implements Audita
 	private Integer productReviewCount;
 
 	@Column(name = "QUANTITY_ORDERED")
+
+
+
+
+/**********************************
+ * CAST-Finding START #1 (2024-02-06 14:04:54.598518):
+ * TITLE: Avoid primitive type wrapper instantiation
+ * DESCRIPTION: Literal values are built at compil time, and their value stored directly in the variable. Literal strings also benefit from an internal mechanism of string pool, to prevent useless duplication, according to the fact that literal string are immutable. On the contrary, values created through wrapper type instantiation need systematically the creation of a new object with many attributes and a life process to manage, and can lead to redondancies for identical values.
+ * STATUS: OPEN
+ * CAST-Finding END #1
+ **********************************/
+
+
 	private Integer productOrdered;
 	
 	@Column(name = "SORT_ORDER")
