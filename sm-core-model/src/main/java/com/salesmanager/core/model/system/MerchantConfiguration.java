78ac659bd8d00c287df93b80546a4048353a7305
Márton Eifert <marton.eifert@metafinanz.de>
2024-02-06 14:04:58 +0100

    QECI-Fix @ MerchantConfiguration.java # 1: Avoid primitive type wrapper instantiation

--- sm-core-model/src/main/java/com/salesmanager/core/model/system/MerchantConfiguration.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/system/MerchantConfiguration.java
@@ -60,6 +60,19 @@ public class MerchantConfiguration extends SalesManagerEntity<Long, MerchantConf
   private String key;
 
   /**
+
+
+
+
+/**********************************
+ * CAST-Finding START #1 (2024-02-06 14:04:57.395342):
+ * TITLE: Avoid primitive type wrapper instantiation
+ * DESCRIPTION: Literal values are built at compil time, and their value stored directly in the variable. Literal strings also benefit from an internal mechanism of string pool, to prevent useless duplication, according to the fact that literal string are immutable. On the contrary, values created through wrapper type instantiation need systematically the creation of a new object with many attributes and a life process to manage, and can lead to redondancies for identical values.
+ * STATUS: OPEN
+ * CAST-Finding END #1
+ **********************************/
+
+
    * activate and deactivate configuration
    */
   @Column(name = "ACTIVE", nullable = true)
