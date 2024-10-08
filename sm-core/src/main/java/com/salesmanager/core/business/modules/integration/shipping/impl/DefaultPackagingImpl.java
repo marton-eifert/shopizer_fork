026bc4985783ba80aae78630f271c0184ab627fa
ALGIT <ALGIT@metafinanz.de>
2024-10-08 16:23:24 +0100

    [8] @ [DefaultPackagingImpl.java] [12 - INIT] [Avoid nested loops]

--- sm-core/src/main/java/com/salesmanager/core/business/modules/integration/shipping/impl/DefaultPackagingImpl.java
+++ sm-core/src/main/java/com/salesmanager/core/business/modules/integration/shipping/impl/DefaultPackagingImpl.java
@@ -382,6 +382,7 @@ public class DefaultPackagingImpl implements Packaging {
 				packages.add(detail);
 			} else if (shippingProduct.getQuantity() > 1) {
 /*** [REF] FINDING-#11: Avoid calling a function in a condition loop (ID: e6b28d2d-a9b9-4994-9129-25394006d0c8) ***/
+/*** [REF] FINDING-#12: Avoid nested loops (ID: f0dc02d0-4ef7-4251-8d2c-292a0c58cbc4) ***/
 				for (int i = 0; i < shippingProduct.getQuantity(); i++) {
 					PackageDetails detail = new PackageDetails();
 					detail.setShippingHeight(h
