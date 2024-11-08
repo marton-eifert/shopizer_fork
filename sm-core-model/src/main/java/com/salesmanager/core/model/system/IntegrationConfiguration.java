2471a7fed32bbd059143b3abdf2ab55705a596b1
ALGIT <ALGIT@metafinanz.de>
2024-11-08 11:48:43 +0000

    [1] @ [IntegrationConfiguration.java] [1 - OPEN] [Avoid nested loops]

--- sm-core-model/src/main/java/com/salesmanager/core/model/system/IntegrationConfiguration.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/system/IntegrationConfiguration.java
@@ -99,6 +99,7 @@ public class IntegrationConfiguration implements JSONAware {
 
 
     if (this.getIntegrationOptions() != null && this.getIntegrationOptions().size() > 0) {
+/*** [START] FINDING-#1: Avoid nested loops (ID: c0317f11-5adb-4aed-b1b3-cba502bcc739) ***/
 
       // JSONObject data = new JSONObject();
       StringBuilder optionDataEntries = new StringBuilder();
@@ -149,6 +150,7 @@ public class IntegrationConfiguration implements JSONAware {
       returnString.append(dataField.toString());
       returnString.append("}");
 
+/*** [END] FINDING-#1: Avoid nested loops (ID: c0317f11-5adb-4aed-b1b3-cba502bcc739) ***/
     }
 
 
